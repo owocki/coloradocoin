d5bf7b70e6270877599133169168c86020166774
madison-kerndt <madison.kerndt@gmail.com>
2018-11-12 11:26:33 -0700

    Fix build by adding yarn and package.json file; minor updates to README; removed console.logs

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
 var ColoradoCoin = artifacts.require("./ColoradoCoin.sol");
-console.log("deploy 1");
 
-module.exports = function(deployer) {
-  deployer.deploy(ColoradoCoin);
+module.exports = function (deployer) {
+    deployer.deploy(ColoradoCoin);
 };
